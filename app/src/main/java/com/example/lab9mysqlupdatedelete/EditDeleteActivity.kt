7a7dc65a94570bf90162c54b7b6e3bc17dc0200b
manow49 <jiravutpa@kkumail.com>
2020-10-18 14:27:50 +0700

    ส่งค้าบ

---
+++ app/src/main/java/com/example/lab9mysqlupdatedelete/EditDeleteActivity.kt
@@ -0,0 +1,19 @@
+package com.example.lab9mysqlupdatedelete
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_edit_delete.*
+import okhttp3.Call
+import okhttp3.Response
+import java.net.CacheResponse
+import javax.security.auth.callback.Callback
+
+class EditDeleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_delete)
+
+    }
+}
